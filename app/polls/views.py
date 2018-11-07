133b43e1f7740e57b1b7ae576649db3cc9060625
Unknown <mairu0514m.o@gmail.com>
2018-11-08 04:52:09 +0900

    viewの作成とurlとの紐づけ

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,6 +1,23 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import Mobile, MobileMessage
 
 
 def index(request):
     return render(request, 'poll/index.html')
+
+
+def user(request, user_id):
+    return render(request, 'poll/user.html', context)
+
+
+def new_user(request):
+    return render(request, 'poll/new_user.html')
+
+
+def send_message(request, user_id):
+    return render(request, 'poll/send_message.html')
+
+
+def tutorial(request):
+    return render(request, 'poll/tutorial.html')
