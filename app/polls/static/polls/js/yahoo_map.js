bb4e57f4af1198d5b173a9a37539e0083416076e
Unknown <mairu0514m.o@gmail.com>
2018-11-08 14:29:19 +0900

    ヤフー地図APIの有効化

---
+++ app/polls/static/polls/js/yahoo_map.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    //地図を初期化
+    var map = new Y.Map("map");
+
+    //コントロールの追加
+    map.addControl(new Y.LayerSetControl());
+    map.addControl(new Y.ZoomControl());
+
+    //地図を表示
+    map.drawMap(new Y.LatLng(35.680840,139.767009), 16 ,Y.LayerSetId.NORMAL);
+}
