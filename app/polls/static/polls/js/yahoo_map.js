a71032ac406186f1ee453c68a4986a6ad09030f9
Unknown <mairu0514m.o@gmail.com>
2018-11-08 18:03:36 +0900

    マップに機能追加
    マップ情報をデータベースからの入手と複数マップ読み込みの実装

--- app/polls/static/polls/js/yahoo_map.js
+++ app/polls/static/polls/js/yahoo_map.js
@@ -1,11 +1,12 @@
-window.onload = function () {
+function load_map (map, lat,lng) {
     //地図を初期化
-    var map = new Y.Map("map");
+
+    var map = new Y.Map(map.toString(10));
 
     //コントロールの追加
     map.addControl(new Y.LayerSetControl());
     map.addControl(new Y.ZoomControl());
 
     //地図を表示
-    map.drawMap(new Y.LatLng(35.680840, 139.767009), 16, Y.LayerSetId.NORMAL);
+    map.drawMap(new Y.LatLng(lat, lng), 16, Y.LayerSetId.NORMAL);
 }
