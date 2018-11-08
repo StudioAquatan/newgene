838febca51a63350b03a5218417c94869fad9adc
Unknown <mairu0514m.o@gmail.com>
2018-11-09 00:32:45 +0900

    index.htmlとviewの修正
    user()をget_user_contextを使ってコンパクトに
    indexからユーザーページに飛べるように

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,30 +1,22 @@
 from django.shortcuts import render
 
-from .models import User, Mobile, MobileMessage
+from .view_func import login, get_user_context
 
 
 def index(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if 'btn_login' in request.POST:
+            user_id = login(username, password)
+            context = get_user_context(user_id)
+            return render(request, 'poll/user.html', context)
+
     return render(request, 'poll/index.html')
 
 
 def user(request, user_id):
-    login_user = User.objects.get(id=user_id)
-    users_mobile = Mobile.objects.filter(user=login_user)
-    message_list = MobileMessage.objects.order_by('reception_time')
-    i = 0
-    latest_message_list=[]
-    for temp_message in message_list:
-        for temp_mobile in users_mobile:
-            if temp_message.mobile == temp_mobile:
-                latest_message_list.append(temp_message)
-                i = i + 1
-                break
-        if i > 5:
-            break
-
-    message = MobileMessage
-    context = {'latest_message_list': latest_message_list,
-               'message': message, 'user_id': user_id}
+    context = get_user_context(user_id)
     return render(request, 'poll/user.html', context)
 
 
