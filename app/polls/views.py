d9d64fd6d6aaec02aa957e09437a40277f286a74
Unknown <mairu0514m.o@gmail.com>
2018-11-08 20:07:33 +0900

    user_idによるメッセージの取得
    ユーザーごとに登録していた端末からのメッセージのみを取得するように修正

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Mobile, MobileMessage
+from .models import User, Mobile, MobileMessage
 
 
 def index(request):
@@ -8,7 +8,20 @@ def index(request):
 
 
 def user(request, user_id):
-    latest_message_list = MobileMessage.objects.order_by('-reception_time')[:5]
+    login_user = User.objects.get(id=user_id)
+    users_mobile = Mobile.objects.filter(user=login_user)
+    message_list = MobileMessage.objects.order_by('reception_time')
+    i = 0
+    latest_message_list=[]
+    for temp_message in message_list:
+        for temp_mobile in users_mobile:
+            if temp_message.mobile == temp_mobile:
+                latest_message_list.append(temp_message)
+                i = i + 1
+                break
+        if i > 5:
+            break
+
     message = MobileMessage
     context = {'latest_message_list': latest_message_list,
                'message': message, 'user_id': user_id}
